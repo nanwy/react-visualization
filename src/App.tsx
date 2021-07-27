9780141333f667fdb11a89feef07fc92d6338099
nanyan <1045578851@qq.com>
2021-07-27 20:28:49 +0800

    feat: initial project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
+  console.log('object');
   return (
     <div className="App">
       <header className="App-header">
